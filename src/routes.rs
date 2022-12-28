266a8b03a9c4971d94e43691b39bc4a1d74c2265
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-28 22:15:31 +0500

    dis is fuking stupid

--- src/routes.rs
+++ src/routes.rs
@@ -1,3 +1,4 @@
+
 use actix_web::{
     get, post,
     web::{Data, Json, Path},
@@ -10,7 +11,13 @@ use crate::AppState;
 
 #[derive(Serialize, FromRow)]
 struct Artist {
+    id: i32,
     name: String,
+}
+
+#[derive(Deserialize)]
+pub struct ArtistBody {
+    pub name: String
 }
 
 #[derive(Serialize, FromRow)]
@@ -23,11 +30,11 @@ struct Album {
 
 #[get("/artists")]
 pub async fn get_artists(state: Data<AppState>) -> impl Responder {
-    match sqlx::query_as::<_, artist>("SELECT id, name FROM artists")
+    match sqlx::query_as::<_, Artist>("SELECT id, name FROM Artist")
         .fetch_all(&state.db)
         .await
     {
-        Ok(users) => HttpResponse::Ok().json(users),
+        Ok(artists) => HttpResponse::Ok().json(artists),
         Err(_) => HttpResponse::NotFound().json("No artists found"),
     }
 }
@@ -35,7 +42,8 @@ pub async fn get_artists(state: Data<AppState>) -> impl Responder {
 
 #[get("/album/{id}")]
 pub async fn get_album(state: Data<AppState>) -> impl Responder {
-    HttpResponse::Ok().json("get album")
+    HttpResponse::Ok().json("create album")
+
 }
 
 #[post("/album/{id}")]
@@ -44,6 +52,16 @@ pub async fn create_album(state: Data<AppState>) -> impl Responder {
 }
 
 #[post("/artists")]
-pub async fn create_artist(state: Data<AppState>) -> impl Responder {
-    HttpResponse::Ok().json("create artist")
+pub async fn create_artist(state: Data<AppState>, body: Json<ArtistBody>) -> impl Responder {
+
+    match sqlx::query_as::<_, Artist>(
+        "INSERT INTO Artist (name) VALUES ($1) RETURNING id, name"
+    )
+        .bind(body.name.to_string())
+        .fetch_one(&state.db)
+        .await
+    {
+        Ok(artist) => HttpResponse::Ok().json(artist),
+        Err(_) => HttpResponse::InternalServerError().json("Failed to create artist"),
+    }
 }
