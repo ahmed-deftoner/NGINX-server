348a026a085a5d33c280d4b2da0b7ca6c83e814c
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-28 18:32:32 +0500

    connection pool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix::fut::ok;
+use actix_web::{web::Data, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+mod routes;
+
+
+pub struct AppState {
+    db: Pool<Postgres>
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("database url needed");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("error building connection pool");
+    
+    ok();
 }
